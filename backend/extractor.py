32418873522d0e5428c21ddb8834ad918adb7d5b
chaykup <siijacob@yahoo.com>
2026-01-22 22:17:27 -0800

    Change OCR extraction to structured model

--- backend/extractor.py
+++ backend/extractor.py
@@ -1,19 +1,19 @@
-from paddleocr import PaddleOCR
+from paddleocr import PaddleOCR, LayoutDetection, PPStructureV3
 
 file = str(input("\nPlease provide filename or path to file: "))
 
 print(f"\nInitializing model...\n")
-ocr = PaddleOCR(
+ocr = PPStructureV3(
+    text_recognition_model_name="en_PP-OCRv4_mobile_rec",
     lang="en",
-    use_textline_orientation=False,
-    ocr_version="PP-OCRv4"
-)
+    use_doc_unwarping=True
+    )
 
 print(f"\nAnalyzing and extracting text from {file}...")
-result = ocr.predict(file)
+result = ocr.predict(input=file)
 result[0].save_to_img("backend/output")
 
 print(f"\n{'=' * 25} Successfully extracted text from {file}! {'=' * 25}\n")
-data = result[0]["rec_texts"]
+data = result[0]
 for item in data:
-    print(f"{item}\n")
+    print(f"- {item}\n")
